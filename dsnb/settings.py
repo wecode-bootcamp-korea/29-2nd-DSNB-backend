75a7978d635c91c7b96aa243bddf31443821075e
OolongTea620 <rlafls9596@gmail.com>
2022-02-15 15:12:58 +0900

    Add::book models
    [add] base timestamp

--- dsnb/settings.py
+++ dsnb/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'django_extensions'
+    'django_extensions',
+    'core'
 ]
 
 MIDDLEWARE = [
