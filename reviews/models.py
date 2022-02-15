035296ab0a3159cdb902bbb6c2074a8e6644d912
OolongTea620 <rlafls9596@gmail.com>
2022-02-15 15:12:58 +0900

    [Add] : review.models, edit book.models

---
+++ reviews/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+from core.models import Base
+from users.models import User
+from books.models import Book
+
+class Review(Base):
+    user        = models.ForeignKey(User, on_delete=models.CASCADE, related_name='writer')
+    book        = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='book')
+    comment     = models.CharField(max_length=600)
+    rating      = models.DecimalField(max_digits = 3, decimal_places = 2)
+    is_spoiler  = models.BooleanField(default=0)
+    
+    class Meta:
+        db_table='reviews'
