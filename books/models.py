698d894bafbf5d7e14256c520e0508bb91705836
sosolyht <sosolyht@gmail.com>
2022-02-16 18:50:34 +0900

    [modify] reviews.model 추가 및 orders.model 일부수정

--- books/models.py
+++ books/models.py
@@ -1,3 +1,4 @@
+from email.mime import image
 import uuid
 
 from django.db import models
@@ -18,16 +19,24 @@ class Author(models.Model):
         db_table = 'authors'
 
 class Book(Base):
-    author      = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
-    catetory    = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
-    title       = models.CharField(max_length=100, blank=False) 
-    publisher   = models.CharField(max_length=200, blank=False)
-    public_date = models.DateTimeField(blank=False)
-    isbn        = models.CharField(max_length=15, blank=False)
-    intro       = models.CharField(max_length=50, blank=False)
+    author       = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
+    catetory     = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+    title        = models.CharField(max_length=100, blank=False)
+    cover_image  = models.URLField(max_length=250, null = True)
+    everage_rate = models.DecimalField(max_digits = 3, decimal_places = 2, null = True)
 
     class Meta:
         db_table = 'books'
+
+class BookDetail():
+    publisher   = models.CharField(max_length=200, blank=False)
+    public_date = models.DateTimeField(blank=False)
+    isbn        = models.CharField(max_length=15, blank=False)
+    intro       = models.CharField(max_length=600, blank=False)
+    book        = models.OneToOneField(Book, on_delete=models.CASCADE, related_name='book_detail')
+
+    class Meta:
+        db_table = 'book_details'
 
 class BookFiles(Base):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable = False)
@@ -45,8 +54,8 @@ class BookOptionType(models.Model):
         db_table = 'book_option_types'
 
 class BookOption(models.Model):
-    book        = models.ForeignKey(Book, on_delete=models.CASCADE, null=True)
-    option      = models.ForeignKey(BookOptionType, on_delete=models.CASCADE, null= True)
+    book        = models.ForeignKey(Book, on_delete=models.CASCADE, null=True, related_name='book_option')
+    option      = models.ForeignKey(BookOptionType, on_delete=models.CASCADE, null= True, related_name='option_name')
     discount    = models.PositiveSmallIntegerField(default=0)
     price       = models.DecimalField(max_digits = 7, decimal_places = 2)
     is_discount = models.BooleanField(default=False)
--- orders/models.py
+++ orders/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 from core.models  import Base
-from books.models import Book
+from books.models import BookOption
 from users.models import User
 
 class OrderStatus(models.Model):
@@ -17,15 +17,15 @@ class PaymentMethod(models.Model):
         db_table = 'payment_methods'
 
 class Order(Base):
-    book_id        = models.ForeignKey(Book, on_delete = models.CASCADE)
+    bookoption_id  = models.ForeignKey(BookOption, on_delete = models.CASCADE)
     order_status   = models.ForeignKey(OrderStatus, on_delete = models.CASCADE)
-    target_user_id = models.ForeignKey(User, on_delete = models.CASCADE, related_name='target_user')
-    user_id        = models.ForeignKey(User, on_delete = models.CASCADE, related_name='origin_user')
-    payment_id     = models.ForeignKey(PaymentMethod, on_delete = models.CASCADE)
+    target_user    = models.ForeignKey(User, on_delete = models.CASCADE, related_name='target_user')
+    user           = models.ForeignKey(User, on_delete = models.CASCADE, related_name='origin_user')
+    payment        = models.ForeignKey(PaymentMethod, on_delete = models.CASCADE, related_name='payment')
     ordered_at     = models.DateTimeField(auto_now_add = True)
     title          = models.CharField(max_length = 200)
     price          = models.DecimalField(max_digits = 7, decimal_places = 2)
     link_url       = models.CharField(max_length = 250)
 
     class Meta:
-        db_table = 'orders'        
+        db_table = 'orders'
