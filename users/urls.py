675c0c0b553ec9207aa45df925552eb0f839e78d
sosolyht <sosolyht@gmail.com>
2022-02-20 19:04:44 +0900

    [add] my library

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import KakaoLoginView
+from .views import KakaoLoginView, UserLibraryView, LibrarySearchView
 
 urlpatterns = [
     path('/kakao-auth' , KakaoLoginView.as_view()),
+    path('/library', UserLibraryView.as_view()),
+    path('', LibrarySearchView.as_view())
 ]
