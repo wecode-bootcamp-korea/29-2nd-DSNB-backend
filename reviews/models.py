b37e96d8a7b40197f08f373837daad515d9b0c98
sosolyht <sosolyht@gmail.com>
2022-02-21 17:50:52 +0900

    [modify] book to review_book

--- reviews/models.py
+++ reviews/models.py
@@ -6,7 +6,7 @@ from books.models import Book
 
 class Review(Base):
     user       = models.ForeignKey(User, on_delete=models.CASCADE, related_name='writer')
-    book       = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='book')
+    book       = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='review_book')
     comment    = models.CharField(max_length=600)
     rating     = models.DecimalField(max_digits = 3, decimal_places = 2)
     is_spoiler = models.BooleanField(default=0)
