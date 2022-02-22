b843f6d53a284fa789f0432b0b4e0bd692239ff1
sosolyht <sosolyht@gmail.com>
2022-02-22 16:43:53 +0900

    [add] mainpage view - /book/<int:book_id> 에서 /<int:book_id> 로 수정 - MainView 에서 SlideView 로 네이밍 수정

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from books.views import BookListView, BookDetailView , NavCategoryView
+from books.views import BookListView, BookDetailView , NavCategoryView, BestSellerView, LocationView, SlideView, SearchView, BookRankView
 urlpatterns = [
-    path('',BookListView.as_view()),
+    path('/', SearchView.as_view()),
     path('/nav', NavCategoryView.as_view()),
-    path('/<int:category_id>',BookListView.as_view()),
-    path('/book/<int:book_id>',BookDetailView.as_view())
+    path('/page', BestSellerView.as_view()),
+    path('/rank', BookRankView.as_view()),
+    path('/slide' , SlideView.as_view()),
+    path('/location', LocationView.as_view()),
+    path('/nation',BookListView.as_view()),
+    path('/<int:book_id>',BookDetailView.as_view()),
+    path('/nation/<int:category_id>',BookListView.as_view())
 ]
