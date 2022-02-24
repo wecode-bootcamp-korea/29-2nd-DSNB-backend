f96f802ffdc4c73f6981312b17cf5bc9a1e6a0e8
sosolyht <sosolyht@gmail.com>
2022-02-24 17:35:16 +0900

    [add] orders views

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http   import JsonResponse
+from django.views  import View
+
+from books.models  import BookOption
+from orders.models import Order
+from users.models  import UserWallet
+
+from users.utils   import login_required
+
+class BeforeBuyView(View):
+    @login_required
+    def get(self, request):
+        user    = request.user
+        wallets = UserWallet.objects.filter(user_id = user)
+        result = [{
+            'cash' : wallet.cash
+        }for wallet in wallets]
+        
+        return JsonResponse({'message' : 'SUCCESS' , 'result' : result}, status = 200)
+
+class BuyBookView(View):
+    @login_required
+    def post(self, request):
+        data = json.loads(request.body)
+
+        BookOption.objects.create(
+            price   = data['price'],
+            book_id = data['book_id']
+        )
+
+        Order.objects.create(
+            user_id         = request.user,
+            target_user_id  = request.user,
+            order_status_id = 1,
+            payment_id      = 1,
+            bookoption_id   = data['book_option'],
+        )
+
+        return JsonResponse({'message' : 'SUCCESS'}, status = 201)
